6b4f5bd1e16a07bcd77feb00407b214311e8692e
cyrusljimenez60 <cyrusljimenez@iskolarngbayan.pup.edu.ph>
2025-01-19 20:03:16 +0800

    Added a logic for climbing movement on ladders

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,12 +5,21 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var on_ladder:bool = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() and !on_ladder:
 		velocity += get_gravity() * delta
-
+	# Check for ladder climb
+	if on_ladder:
+		if Input.is_action_pressed("ui_down"):
+			velocity.y = SPEED*delta*10
+		elif Input.is_action_pressed("ui_up"):
+			velocity.y = -SPEED*delta*10
+		else:
+			velocity.y = 0
+			
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -38,5 +47,12 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
 	move_and_slide()
+func _on_ladder_body_entered(body: Node2D) -> void:
+	if "Player" in body.name:
+		on_ladder = true
+		
+func _on_ladder_body_exited(body: Node2D) -> void:
+	if "Player" in body.name:
+		on_ladder = false
